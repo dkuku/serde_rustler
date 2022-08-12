48e446e9c12652c680031307735751e4b6342e9a
Daniel Flanagan <daniel.flanagan@divvypay.com>
2022-08-12 16:52:02 -0500

    Upgrade Rustler to 0.25

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SerdeRustler.Mixfile do
   use Mix.Project
 
   @name         :serde_rustler
-  @version      "0.0.1"
+  @version      "0.0.2"
   @description  """
   """
   @github       "https://github.com/sunny-g/serde_rustler"
